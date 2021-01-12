be749a828990cbf3cd9d168ad95e85fd3ab939c8
Benedict <hackgh94@gmail.com>
2021-01-12 23:26:00 +0000

    add code in files types: .java,.yml remove code in files types: .java,.yml

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cclvqp {
     public static void main() {
         System.out.println("vxvpa");
         System.out.println("hvnft");
-        System.out.println("rrikp");
+        System.out.println("dabbo");
     }
 }
