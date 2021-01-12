48cfa429b78269b0b148a2d1607925419eb7815f
Benedict <hackgh94@gmail.com>
2021-01-12 23:56:17 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cclvqp {
     public static void main() {
         System.out.println("vxvpa");
         System.out.println("hvnft");
-        System.out.println("dabbo");
+        System.out.println("ycywk");
     }
 }
